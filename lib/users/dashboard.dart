4a51f1a023df2f780f99a462110d66b611bc735a
sanatani11 <raushan.ranjan.863@gmail.co>
2024-01-18 18:12:22 +0530

    v5.1/18/24

--- lib/users/dashboard.dart
+++ lib/users/dashboard.dart
@@ -1,49 +1,71 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  LocationData? currentLocation;
+
+  void getCurrentLocation() async {
+    Location location = Location();
+    LocationData? locationData = await location.getLocation();
+    if (locationData != null) {
+      setState(() {
+        currentLocation = locationData;
+      });
+    } else {
+      print('kuchh nahi mila yaar');
+    }
+  }
+
+  @override
+  void initState() {
+    getCurrentLocation();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Dashboard'),
-          actions: [
-            IconButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              icon: Icon(
-                Icons.exit_to_app,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-            ),
-          ],
-        ),
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.all(16),
-          child: Center(
-            child: Column(
-              children: [
-                Container(
-                  height: 200,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.rectangle,
-                    borderRadius: const BorderRadius.vertical(),
-                    border: Border.all(
-                        width: 2,
-                        color: const Color.fromARGB(255, 229, 225, 225),
-                        style: BorderStyle.solid),
-                  ),
-                  child: const Center(
-                    child: Text('Tap to add location.'),
-                  ),
-                )
-              ],
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Theme.of(context).colorScheme.primary,
             ),
           ),
-        ));
+        ],
+      ),
+      body: currentLocation == null
+          ? const Center(
+              child: Text('Loading.'),
+            )
+          : GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: LatLng(
+                    currentLocation!.latitude!, currentLocation!.longitude!),
+                zoom: 20,
+              ),
+              markers: {
+                Marker(
+                  markerId: const MarkerId("currentLocation"),
+                  position: LatLng(
+                      currentLocation!.latitude!, currentLocation!.longitude!),
+                ),
+              },
+            ),
+    );
   }
 }

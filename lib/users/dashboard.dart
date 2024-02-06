699c196cfbac15ae2ce36175cf547b0d16aeb685
sanatani11 <raushan.ranjan.863@gmail.co>
2024-02-06 18:13:16 +0530

    v7.2/6/24

--- lib/users/dashboard.dart
+++ lib/users/dashboard.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:cycle_kiraya/widgets/divider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -32,6 +35,8 @@ class _DashboardScreenState extends State<DashboardScreen> {
     super.initState();
   }
 
+  Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,24 +54,174 @@ class _DashboardScreenState extends State<DashboardScreen> {
           ),
         ],
       ),
-      body: currentLocation == null
-          ? const Center(
-              child: Text('Loading.'),
-            )
-          : GoogleMap(
-              initialCameraPosition: CameraPosition(
-                target: LatLng(
-                    currentLocation!.latitude!, currentLocation!.longitude!),
-                zoom: 15,
-              ),
-              markers: {
-                for (final location in availableLocation)
-                  Marker(
-                    markerId: MarkerId(location.id),
-                    position: LatLng(location.lat, location.lng),
+      body: Stack(
+        children: [
+          currentLocation == null
+              ? const Center(
+                  child: Text('Loading.'),
+                )
+              : GoogleMap(
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(currentLocation!.latitude!,
+                        currentLocation!.longitude!),
+                    zoom: 15,
                   ),
-              },
+                  markers: {
+                    for (final location in availableLocation)
+                      Marker(
+                        markerId: MarkerId(location.id),
+                        position: LatLng(location.lat, location.lng),
+                      ),
+                  },
+                  onMapCreated: (controller) {
+                    _controller.complete(controller);
+                  },
+                ),
+          Positioned(
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.0,
+            child: Container(
+              height: 350.0,
+              //color: Colors.yellowAccent,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black,
+                      spreadRadius: 0.5,
+                      blurRadius: 10,
+                      offset: Offset(0.5, 0.5))
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24,
+                  vertical: 18,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      'Hellooo',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    const Text(
+                      'Where to?',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8),
+                        boxShadow: const [
+                          BoxShadow(
+                              color: Colors.grey,
+                              spreadRadius: 0.5,
+                              blurRadius: 10,
+                              offset: Offset(0.5, 0.5))
+                        ],
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.search,
+                              color: Colors.blueAccent,
+                            ),
+                            SizedBox(
+                              width: 12,
+                            ),
+                            Text(
+                              'Search Drop off',
+                              style: TextStyle(fontSize: 15),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    const Row(
+                      children: [
+                        Icon(
+                          Icons.home,
+                          color: Colors.grey,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Add Home',
+                            ),
+                            SizedBox(
+                              height: 4.0,
+                            ),
+                            Text(
+                              "Address where you live.",
+                              style: TextStyle(
+                                  color: Colors.black54, fontSize: 12.0),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const DividerWidget(),
+                    const SizedBox(
+                      height: 8.0,
+                    ),
+                    const Row(
+                      children: [
+                        Icon(
+                          Icons.work,
+                          color: Colors.grey,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Add Work',
+                            ),
+                            SizedBox(
+                              height: 4.0,
+                            ),
+                            Text(
+                              "Address where you work.",
+                              style: TextStyle(
+                                  color: Colors.black54, fontSize: 12.0),
+                            ),
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
             ),
+          ),
+        ],
+      ),
     );
   }
 }
